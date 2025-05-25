8ec85d3ff991b8092ac9f27f8591f63dbced62d5
quantumspawner <jet22854111@gmail.com>
2025-05-25 22:32:36 +0800

    minor restructure

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,75 @@
 mod app;
 
 #[cfg(not(target_arch = "wasm32"))]
-mod main_native;
-#[cfg(not(target_arch = "wasm32"))]
-use main_native::*;
+pub fn main() -> eframe::Result {
+    use eframe::{egui_wgpu, wgpu};
+    use std::sync::Arc;
+
+    env_logger::init();
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([800.0, 600.0])
+            .with_min_inner_size([800.0, 600.0]),
+        wgpu_options: egui_wgpu::WgpuConfiguration {
+            wgpu_setup: egui_wgpu::WgpuSetup::CreateNew(egui_wgpu::WgpuSetupCreateNew {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                device_descriptor: Arc::new(|adapter| {
+                    let base_limits = if adapter.get_info().backend == wgpu::Backend::Gl {
+                        wgpu::Limits::downlevel_webgl2_defaults()
+                    } else {
+                        wgpu::Limits::default()
+                    };
+
+                    wgpu::DeviceDescriptor {
+                        label: Some("egui wgpu device"),
+                        required_features: wgpu::Features::default(),
+                        required_limits: wgpu::Limits {
+                            max_storage_buffer_binding_size: 512_u32 << 20, // 512 MB
+                            ..base_limits
+                        },
+                        memory_hints: wgpu::MemoryHints::default(),
+                    }
+                }),
+                ..Default::default()
+            }),
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Ray Tracer",
+        native_options,
+        Box::new(|cc| Ok(Box::new(app::App::new(cc)))),
+    )
+}
 
 #[cfg(target_arch = "wasm32")]
-mod main_wasm;
-#[cfg(target_arch = "wasm32")]
-use main_wasm::*;
+pub fn main() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let _start_result = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|cc| Ok(Box::new(app::App::new(cc)))),
+            )
+            .await;
+    });
+}
