df6a354053858865b44e8041abdd1bf09c51e5b8
quantumspawner <jet22854111@gmail.com>
2025-05-28 10:39:25 +0800

    bug fix

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,6 @@ pub mod ray_tracer;
 pub use eframe::wgpu;
 
 #[cfg(not(target_arch = "wasm32"))]
-pub use std::time as timer;
+pub use std::time as time;
 #[cfg(target_arch = "wasm32")]
 pub use web_time as time;
